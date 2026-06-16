bb856e641b1e1cb7ec113ad251fb69a6a8b35509
Anshuman Solanki <kirtikamlesh48@gmail.com>
2026-06-16 16:57:45 +0530

    initial W2 deliverables setup

---
+++ src/Hero.tsx
@@ -0,0 +1,10 @@
+function Hero() {
+  return (
+    <section>
+      <h1>Welcome</h1>
+      <p>This is the Hero Section</p>
+    </section>
+  );
+}
+
+export default Hero;
---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer>
+      <p>Footer Section</p>
+    </footer>
+  );
+}
+
+export default Footer;
